41855bb73d250bc7ecdc60edf5c52cb09372f03b
Skrievo <gebertundco@gmail.com>
2025-11-20 00:16:06 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+
+game 'rdr3'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'Skrievo Development'
+
+version '1.0.0'
+
+server_scripts {
+    'server/main.lua'
+}
+
+client_scripts {
+    'client/warmenu.lua',
+    'client/main.lua',
+    'client/functions.lua'
+}
+
+shared_scripts {
+    'config.lua',
+    'locale.lua',
+    'locales/es.lua',
+    'locales/en.lua'
+}
